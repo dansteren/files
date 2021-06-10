a92164a13a1dd19a56f9d2e1bcca554963755d45
Dan Steren <daniel.d.steren@gmail.com>
2021-06-09 23:12:03 -0600

    Add prettier

--- canisters/frontend/types/index.d.ts
+++ canisters/frontend/types/index.d.ts
@@ -1,39 +1,39 @@
 export type User = {
-    id: string;
-    username: string;
-    blogPosts: ReadonlyArray<BlogPost>;
+  id: string;
+  username: string;
+  blogPosts: ReadonlyArray<BlogPost>;
 };
 
 export type BlogPost = {
-    id: string;
-    publishedAt?: Date;
-    title: string;
-    author: User;
+  id: string;
+  publishedAt?: Date;
+  title: string;
+  author: User;
 };
 
 export type BlogPostInputTexts = {
-    [userId: string]: string;
+  [userId: string]: string;
 };
 
 export type BlogPostsAreSaving = {
-    [userId: string]: boolean;
+  [userId: string]: boolean;
 };
 
 export interface UsernameInputTextChangedEvent extends CustomEvent {
-    detail: Readonly<{
-        usernameInputText: string;
-    }>;
+  detail: Readonly<{
+    usernameInputText: string;
+  }>;
 }
 
 export interface CreateBlogPostEvent extends CustomEvent {
-    detail: Readonly<{
-        userId: string;
-    }>;
+  detail: Readonly<{
+    userId: string;
+  }>;
 }
 
 export interface BlogPostInputTextChangedEvent extends CustomEvent {
-    detail: Readonly<{
-        userId: string;
-        blogPostInputText: string;
-    }>;
+  detail: Readonly<{
+    userId: string;
+    blogPostInputText: string;
+  }>;
 }
